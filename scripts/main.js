0547a6f2ef000e014817c537afb4eff0050f7dd6
Simonova <zonnya@mail.ru>
2017-10-23 20:44:57 +0300

    2,3 screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
+
   $(".slider li a").click(function(){
+    $(".slider li").removeClass('active');
     var targetID = $(this).attr("href");
+    $('.slider li a[href=' + targetID +']').closest('li').addClass('active');
     var $target = $(targetID);
     $(".block").not($target).fadeOut();
     $target.fadeIn();
     return false;
   });
   $(".slider li").click(function(){
+    $(".slider li").removeClass('active');
     var targetID = $(this).find("a").attr("href");
+    $('.slider li a[href=' + targetID +']').closest('li').addClass('active');
     var $target = $(targetID);
     $(".block").not($target).fadeOut();
     $target.fadeIn();
     return false;
   });
+
+  if ($(".slider li.active").length) {
+    $(".slider li.active").click();
+  }
+  else{
+    $('.block:first-of-type').fadeIn();
+    var block_id = $('.block:first-of-type').attr('id');
+    $('.slider li a[href="#'+ block_id +'"]').closest('li').addClass('active');
+  }
 });
